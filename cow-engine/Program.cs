38bcf54601253f745824f3f5259c07aa29ad04b6
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-04-03 11:31:54 +0300

    Initial commit

---
+++ cow-engine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cow_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
