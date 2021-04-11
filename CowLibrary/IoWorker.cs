3dab7d7ed3200c4ad315c2ac46b5c02685ec4d07
dmytro.stolynets <Chakaramba@gmail.com>
2021-04-11 19:13:58 +0300

    Creating output directory when necessary

--- CowLibrary/IoWorker.cs
+++ CowLibrary/IoWorker.cs
@@ -25,6 +25,8 @@ namespace CowEngine.ImageWorker
         public void Write(byte[] bytes, string path)
         {
             path = path.Replace('/', Path.DirectorySeparatorChar);
+            var fileInfo = new FileInfo(path);
+            fileInfo.Directory?.Create();
             File.WriteAllBytes(path, bytes);
         }
     }
