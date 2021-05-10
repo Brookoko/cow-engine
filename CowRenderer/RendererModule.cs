01f48bc3dcfbb543f468ba66fa4ca6583c402a60
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-05-10 17:54:30 +0300

    Enable tree raytracer

--- CowRenderer/RendererModule.cs
+++ CowRenderer/RendererModule.cs
@@ -10,7 +10,7 @@ namespace CowRenderer
         public void Prepare(DiContainer container)
         {
             container.Bind<IIntegrator>().To<MaterialIntegrator>().ToSingleton();
-            container.Bind<IRaycaster>().To<SimpleRaycaster>().ToSingleton();
+            container.Bind<IRaycaster>().To<TreeRaycaster>().ToSingleton();
             container.Bind<IRenderer>().To<MultithreadRenderer>().ToSingleton();
             container.Bind<ThreadRenderer>().To<MultiRayThreadRenderer>();
             container.Bind<RenderConfig>().To<RenderConfig>().ToSingleton();
