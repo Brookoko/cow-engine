56bf3de6b4e112e0d0ce6b72e531b65ce926d7fd
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-06-02 01:31:05 +0300

    Fix reflection

--- CowLibrary/Models/Fresnel/DielectricFresnel.cs
+++ CowLibrary/Models/Fresnel/DielectricFresnel.cs
@@ -13,15 +13,17 @@ namespace CowLibrary
             cosThetaI = Math.Clamp(cosThetaI, -1, 1);
             var etaI = this.etaI;
             var etaT = this.etaT;
-            var entering = cosThetaI <= 0;
+            var entering = cosThetaI > 0;
             if (entering)
             {
                 (etaI, etaT) = Mathf.Swap(etaI, etaT);
                 cosThetaI = Math.Abs(cosThetaI);
             }
             
-            var sinThetaI = Math.Sqrt(1 - cosThetaI * cosThetaI);
+            var sinThetaI = Math.Sqrt(Math.Max(0, 1 - cosThetaI * cosThetaI));
             var sinThetaT = etaI / etaT * sinThetaI;
+            if (sinThetaT >= 0) return 1;
+            
             var cosThetaT = Math.Sqrt(Math.Max(0, 1 - sinThetaT * sinThetaT));
             
             var rParl = (etaT * cosThetaI - etaI * cosThetaT) / (etaT * cosThetaI + etaI * cosThetaT);
