f2395bd507eeee90bd1c5de37ac17dde325a3d7e
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2022-05-14 13:28:12 +0300

    Add ILGPU renderer project

---
+++ ILGPURenderer/GpuKernel.cs
@@ -0,0 +1,45 @@
+﻿namespace ILGPURenderer
+{
+    using System;
+    using ILGPU;
+    using ILGPU.Runtime;
+    using ILGPU.Runtime.CPU;
+    using ILGPU.Runtime.Cuda;
+
+    public class GpuKernel : IDisposable
+    {
+        private readonly Context context;
+        private readonly Accelerator accelerator;
+
+        public GpuKernel(KernelMode mode)
+        {
+            context = Context.Create(b => b.Cuda().EnableAlgorithms().Assertions());
+            accelerator = mode.GetAccelerator(context);
+        }
+
+        public void Dispose()
+        {
+            accelerator.Dispose();
+            context.Dispose();
+        }
+    }
+
+    public enum KernelMode
+    {
+        Gpu,
+        Cpu
+    }
+
+    public static class KernelModeExtensions
+    {
+        public static Accelerator GetAccelerator(this KernelMode mode, Context context)
+        {
+            return mode switch
+            {
+                KernelMode.Gpu => context.CreateCudaAccelerator(0),
+                KernelMode.Cpu => context.CreateCPUAccelerator(0),
+                _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, $"Unknown mode {mode}")
+            };
+        }
+    }
+}
---
+++ ILGPURenderer/ILGPURenderer.cs
@@ -0,0 +1,18 @@
+﻿namespace ILGPURenderer
+{
+    using CowLibrary;
+    using CowRenderer;
+
+    public class ILGPURenderer : IRenderer
+    {
+        public Image Render(Scene scene)
+        {
+            var camera = scene.MainCamera;
+            var w = camera.width;
+            var h = camera.height;
+            var image = new Image(w, h);
+            using var gpuKernel = new GpuKernel(KernelMode.Gpu);
+            return image;
+        }
+    }
+}
