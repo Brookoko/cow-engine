f89ce5439de4f0515f9baca63d79a440303924b0
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2022-05-18 19:18:43 +0300

    Add support for different lights

--- ILGPURenderer/LocalRender/LocalIntegrator.cs
+++ ILGPURenderer/LocalRender/LocalIntegrator.cs
@@ -1,86 +1,126 @@
 ﻿namespace ILGPURenderer;
 
 using System;
-using System.Linq;
 using System.Numerics;
 using CowLibrary;
-using CowLibrary.Lights;
+using CowLibrary.Lights.Models;
 using CowLibrary.Mathematics.Sampler;
-using CowRenderer;
 using Data;
-using LocalRender;
+using ILGPU;
 
 public readonly struct LocalIntegrator
 {
     private readonly LocalSampler sampler;
     private readonly LocalRaycaster raycaster;
-    private readonly EnvironmentIntegrator environmentIntegrator;
 
     public LocalIntegrator(LocalSampler sampler, LocalRaycaster raycaster)
     {
         this.sampler = sampler;
         this.raycaster = raycaster;
-        environmentIntegrator = new EnvironmentIntegrator(sampler, raycaster);
     }
 
     public Color GetColor(in SceneView sceneView, in RenderData renderData, in RayHit hit, in Ray ray)
     {
         if (!hit.HasHit)
         {
-            return GetLightColor(in sceneView, in hit, in ray);
+            return GetLightColor(in sceneView, in ray);
         }
         var color = Color.Black;
         for (var i = 0; i < sceneView.light.environmentLights.Length; i++)
         {
             var light = sceneView.light.environmentLights[i];
             var matrix = sceneView.light.GetMatrix(light.Id);
-            color += environmentIntegrator.GetLighting(in sceneView, in renderData, in hit, in ray, in light, in matrix, 0);
+            color += GetLighting(in sceneView, in renderData, in hit, in ray, in light, in matrix, 0);
         }
         return color;
     }
 
-    private Color GetLightColor(in SceneView sceneView, in RayHit hit, in Ray ray)
+    private Color GetLightColor(in SceneView sceneView, in Ray ray)
     {
         var color = Color.Black;
-        for (var i = 0; i < sceneView.light.environmentLights.Length; i++)
+        color += GetLightColor(in sceneView.light.environmentLights, in ray.direction);
+        return color;
+    }
+
+    private Color GetLightColor<T>(in ArrayView<T> lights, in Vector3 direction)
+        where T : unmanaged, ILightModel
+    {
+        var color = Color.Black;
+        for (var i = 0; i < lights.Length; i++)
         {
-            color += sceneView.light.environmentLights[i].Sample(in ray.direction, sampler.CreateSample());
+            color += lights[i].Sample(in direction, sampler.CreateSample());
         }
         return color;
     }
 
-    // private Color GetIndirectLighting(in Surfel surfel, Light light, int depth)
-    // {
-    //     var result = Color.Black;
-    //     var n = RenderConfig.numberOfRayPerLight;
-    //     for (var i = 0; i < n; i++)
-    //     {
-    //         var sample = SamplerProvider.Sampler.CreateSample();
-    //         var f = surfel.material.Sample(in surfel.hit.normal, in surfel.ray, out var wi, in sample, out var pdf);
-    //         if (pdf > 0 && f > 0)
-    //         {
-    //             result += f * pdf * Trace(in surfel, light, wi, depth);
-    //         }
-    //     }
-    //     return result / RenderConfig.numberOfRayPerLight;
-    // }
-    //
-    // private Color Trace(in Surfel surfel, Light light, Vector3 direction, int depth)
-    // {
-    //     if (depth >= RenderConfig.rayDepth)
-    //     {
-    //         return Color.Black;
-    //     }
-    //     var sign = Math.Sign(Vector3.Dot(surfel.hit.normal, direction));
-    //     var position = surfel.hit.point + sign * surfel.hit.normal * RenderConfig.bias;
-    //     var surfelHit = Raycaster.Raycast(new Ray(position, direction));
-    //     if (surfelHit.hit.HasHit)
-    //     {
-    //         return GetLighting(in surfelHit, light, depth + 1);
-    //     }
-    //     var lightning = light.Sample(in direction);
-    //     var dot = Vector3.Dot(surfel.hit.normal, direction);
-    //     dot = Math.Max(dot, 0);
-    //     return surfel.material.Color * lightning * dot;
-    // }
+    public Color GetLighting<T>(in SceneView sceneView, in RenderData renderData, in RayHit hit, in Ray ray,
+        in T light, in Matrix4x4 matrix, int depth)
+        where T : struct, ILightModel
+    {
+        // return GetDirectLighting(in sceneView, in hit, in ray, in light, in matrix);
+        return GetDirectLighting(in sceneView, in hit, in ray, in light, in matrix) +
+               GetIndirectLighting(in sceneView, in renderData, in hit, in ray, in light, in matrix, depth);
+    }
+
+    private Color GetDirectLighting<T>(in SceneView sceneView, in RayHit hit, in Ray ray, in T light,
+        in Matrix4x4 matrix)
+        where T : struct, ILightModel
+    {
+        var shading = light.GetShadingInfo(in hit, in matrix, sampler.CreateSample());
+        var color = sceneView.material.GetMaterialColor(in hit.id, in ray.direction, in shading.direction);
+        var dot = Vector3.Dot(hit.normal, shading.direction);
+        dot = Math.Max(dot, 0);
+        var multiplier = TraceShadowRay(in sceneView, in hit, in shading.direction, shading.distance);
+        return multiplier * color * shading.color * dot;
+    }
+
+    private float TraceShadowRay(in SceneView sceneView, in RayHit hit, in Vector3 direction, float distance)
+    {
+        var position = hit.point + hit.normal * Const.Bias;
+        var surfaceHit = raycaster.Raycast(in sceneView.mesh, new Ray(position, direction));
+        return surfaceHit.HasHit && surfaceHit.t < distance ? 0 : 1;
+    }
+
+    private Color GetIndirectLighting<T>(in SceneView sceneView, in RenderData renderData, in RayHit hit, in Ray ray,
+        in T light, in Matrix4x4 matrix, int depth)
+        where T : struct, ILightModel
+    {
+        var result = Color.Black;
+        var n = renderData.numberOfRayPerMaterial;
+        for (var i = 0; i < n; i++)
+        {
+            var sample = sampler.CreateSample();
+            var f = sceneView.material.Sample(in hit.id, in hit.normal, in ray.direction, out var wi, in sample,
+                out var pdf);
+            if (pdf > 0 && f > 0)
+            {
+                result += f * pdf * Trace(in sceneView, in renderData, in light, in hit, in wi, in matrix, depth);
+            }
+        }
+        return result / renderData.numberOfRayPerMaterial;
+    }
+
+    private Color Trace<T>(in SceneView sceneView, in RenderData renderData, in T light, in RayHit hit,
+        in Vector3 direction, in Matrix4x4 matrix, int depth)
+        where T : struct, ILightModel
+    {
+        if (depth >= renderData.rayDepth)
+        {
+            return Color.Black;
+        }
+        var sign = Vector3.Dot(hit.normal, direction) >= 0 ? 1 : -1;
+        var position = hit.point + sign * hit.normal * Const.Bias;
+        var ray = new Ray(position, direction);
+        var surfelHit = raycaster.Raycast(in sceneView.mesh, in ray);
+        if (surfelHit.HasHit)
+        {
+            return Color.Black;
+            return GetLighting(in sceneView, in renderData, in hit, in ray, in light, in matrix, depth + 1);
+        }
+        var lightning = light.Sample(in direction, sampler.CreateSample());
+        var dot = Vector3.Dot(hit.normal, direction);
+        dot = Math.Max(dot, 0);
+        var color = sceneView.material.GetMaterialRawColor(in hit.id);
+        return color * lightning * dot;
+    }
 }
