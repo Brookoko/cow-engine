954b5814ea3f63411350558fc1d381ae3dac1500
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2022-05-18 19:28:43 +0300

    Remove old kernels

--- ILGPURenderer/ILGPUModule.cs
+++ ILGPURenderer/ILGPUModule.cs
@@ -11,9 +11,6 @@
             var gpuKernel = new GpuKernel(KernelMode.Gpu);
             container.Bind<GpuKernel>().ToInstance(gpuKernel);
             container.Bind<IRenderer>().WithName(KernelMode.Gpu).To<ILGPURenderer>().ToSingleton();
-            container.Bind<IPrimaryRayKernel>().To<PrimaryRayKernel>().ToSingleton();
-            container.Bind<IHitKernel>().To<HitKernel>().ToSingleton();
-            container.Bind<IColorKernel>().To<ColorKernel>().ToSingleton();
             container.Bind<IRenderKernel>().To<RenderKernel>().ToSingleton();
             container.Bind<ILocalSamplerProvider>().To<LocalSamplerProvider>().ToSingleton();
             container.Bind<ISceneConverter>().To<SceneConverter>().ToSingleton();
