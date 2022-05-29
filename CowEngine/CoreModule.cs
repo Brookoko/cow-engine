47e475dfc30741a0efe1960547ed5b1157221b92
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2022-05-29 16:41:13 +0300

    Change io worker

--- CowEngine/CoreModule.cs
+++ CowEngine/CoreModule.cs
@@ -9,8 +9,11 @@ namespace CowEngine
 
         public void Prepare(DiContainer container)
         {
+            container.Bind<IBytesReader>().To<SimpleBytesReader>().ToSingleton();
+            container.Bind<IBytesWriter>().ToInstance(new FilesBypassingWriter("_"));
+            container.Bind<IIoWorker>().To<IoWorker>().ToSingleton();
+            
             container.BindInterfacesTo<ArgumentsParser>().ToSingleton();
-            container.BindInterfacesTo<IoWorker>().ToSingleton();
             container.BindInterfacesTo<Watch>().ToSingleton();
             container.BindInterfacesTo<SceneLoader>().ToSingleton();
             container.BindInterfacesTo<BasicFlow>().ToSingleton();
